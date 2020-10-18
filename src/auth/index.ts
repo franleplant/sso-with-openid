7d2e8472c93c66fb8cb84baea8f482fcf6d15f76
Fran Guijarro <franleplant@gmail.com>
2020-10-18 11:51:38 -0300

    auth state

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,3 +1,44 @@
+import debugFactory from 'debug'
+import { Issuer, custom } from "openid-client";
+
 export * from "./middleware";
 export { default as routes } from "./routes";
 export * from "./session";
+
+
+// A simple way of debugging openid-client requests
+const debug = debugFactory("myapp");
+
+custom.setHttpOptionsDefaults({
+  headers: {
+    // temporary workaround
+    // for some reason the default user agent that this lib uses
+    // breaks costar CA, so we need to force it to something else,
+    // TODO maybe use something better than Postman's
+    "User-Agent": "PostmanRuntime/7.26.5",
+  },
+
+  hooks: {
+    beforeRequest: [
+      (options) => {
+        const { method, url, headers, body, form } = options;
+        debug(`>>> Request %s %s`, method.toUpperCase(), url.href);
+        debug(`Headers: %O`, headers);
+        debug(`Body %O`, body || form);
+      },
+    ],
+    afterResponse: [
+      (response) => {
+        const { statusCode, headers, body } = response;
+        const { method, url } = response.request.options;
+
+        debug(`<<< Response %s %s`, method.toUpperCase(), url.href);
+        debug(`Status %s`, statusCode);
+        debug(`Headers: %O`, headers);
+        debug(`Body %O`, body);
+        return response;
+      },
+    ],
+  },
+});
+
--- src/auth/routes.ts
+++ src/auth/routes.ts
@@ -1,7 +1,25 @@
 import { Router } from "express";
+import {generators} from 'openid-client'
 import { setSessionCookie, clearSessionCookie } from "./cookie";
 import { serialize } from "./session";
 import { getDomain } from "./middleware";
+
+interface IState {
+  backToPath: string;
+  bytes: string;
+}
+
+function serializeAuthState(state: Partial<IState>): string {
+  // probably you would base64 encode this
+  return JSON.stringify({
+    ...state,
+    bytes: generators.state()
+  })
+}
+
+function deserializeAuthState(value: string): IState {
+  return JSON.parse(value)
+}
 
 /*
   This is a simple middleware that hosts all the routes
@@ -17,35 +35,40 @@ import { getDomain } from "./middleware";
  */
 export default function authRoutesMiddleware(): Router {
   const router = Router();
-  const BACKTO_COOKIE = "backTo";
+  const STATE_COOKIE = "state";
 
   router.get("/auth/login", function (req, res, next) {
-    const backTo = req.query.backTo;
+    const backToPath = req.query.backTo as string || "/private";
+    const state = serializeAuthState({backToPath})
     const authUrl = req.app.authClient!.authorizationUrl({
       scope: "openid email profile",
+      state,
     });
 
-    res.cookie(BACKTO_COOKIE, backTo);
+    res.cookie(STATE_COOKIE, state);
     res.redirect(authUrl);
   });
 
   router.get("/auth/callback", async (req, res, next) => {
-    const backTo = req.cookies[BACKTO_COOKIE] || "/private";
+    const state = req.cookies[STATE_COOKIE];
     const client = req.app.authClient;
 
     const params = client!.callbackParams(req);
     const tokenSet = await client!.callback(
       `${getDomain()}/auth/callback`,
-      params
+      params,
+      {state}
     );
     const user = await client!.userinfo(tokenSet);
 
     const sessionCookie = serialize({ tokenSet, user });
     setSessionCookie(req, sessionCookie);
 
-    res.redirect(backTo);
+    const {backToPath} = deserializeAuthState(state)
+    res.redirect(backToPath);
   });
 
+  //TODO
   router.get("/auth/logout", async (req, res, next) => {
     const client = req.app.authClient;
     const tokenSet = req.session?.tokenSet;
